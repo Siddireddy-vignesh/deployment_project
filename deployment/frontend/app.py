5d91e8cc58df58006a1f7d5ecd254a4e5eaa0317
Siddireddy-vignesh <siddireddyvigneshwarreddy@gmail.com>
2026-07-24 07:13:42 +0530

    requirements.txt file is added to the backend

--- deployment/frontend/app.py
+++ deployment/frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests as req
 
-
+backend_server_location = "http://127.0.0.1:8000"
 
 n = st.text_input("Name")
 e = st.text_input("email")
@@ -15,7 +15,7 @@ if register_btn:
         "password":p
     }
 
-    res = req.post("http://127.0.0.1:8000/register",json = payload)
+    res = req.post(f"{backend_server_location}/register",json = payload)
 
-if res.status_code == 200:
-    st.write(res.json())
+    if res.status_code == 200:
+        st.write(res.json())
